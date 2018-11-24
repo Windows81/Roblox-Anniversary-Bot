e9b78a2a3ec082810dcfcf808288526f7da8e541
1630228 <nastyahmede@gmail.com>
2018-11-24 08:26:20 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 const http=require('http');
+const request = require('request');
 const PORT=process.env.PORT || 5000;
-var c='';
+
 const server=http.createServer((req,res)=>{
   res.statusCode=200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end(c);
+  res.end(request!=null?'1':'0');
 });
 server.listen(PORT,()=>{
   console.log(`Server running on ${PORT}/`);
-});
-
-const request = require('request');
-
-request('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', { json: true }, (err, res, body) => {
-  if (err) { return console.log(err); }
-  c=body.url;
-  console.log(body.explanation);
 });
