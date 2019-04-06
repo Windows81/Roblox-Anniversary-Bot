05d40a7f30ec810bf150230c31b5b627b2ffb211
1630228 <nastyahmede@gmail.com>
2019-04-06 06:39:40 +0000

    Update index.js

--- index.js
+++ index.js
@@ -11,5 +11,7 @@ server.listen(PORT,()=>{
   console.log(`Server running on ${PORT}/`);
 });
 
-for(var c=1630228;c<1630229;c++)
+for(var c=1630228;c<1630229;c++){
 	request.get(`https://www.roblox.com/users/${c}/profile`,(e,r,b)=>{console.log(b.length);})
+	console.log(c);
+}
