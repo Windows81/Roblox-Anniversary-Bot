1b7217d726f6b802ee49ffb203f8fe465ad5ba47
1630228 <nastyahmede@gmail.com>
2019-04-06 05:56:17 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const PORT=process.env.PORT || 5000;
 const server=http.createServer((req,res)=>{
 	res.statusCode=200;
 	res.setHeader('Content-Type', 'text/plain');
-	req.post({
+	req({
+		method:'POST',
 		url:'https://presence.roblox.com/v1/presence/users',
-		body:'{"userIds":[1630227,1630228]}','content-type':'application/json'},function(e,r,body){console.log(e);})
+		body:'{"userIds":[1630227,1630228]}','content-type':'application/json'},
+		function(e,r,body){console.log(r.statusCode);})
 });
 server.listen(PORT,()=>{
   console.log(`Server running on ${PORT}/`);
