1940bbd635cca2501dd34f5b0d81a864ecd88056
1630228 <nastyahmede@gmail.com>
2019-04-06 06:56:20 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http=require('http');
-const request=require('request');
+const request=require('request-promise-native');
 const PORT=process.env.PORT || 5000;
 
 const server=http.createServer((req,res)=>{
@@ -11,6 +11,6 @@ server.listen(PORT,()=>{
   console.log(`Server running on ${PORT}/`);
 });
 
-for(var c=1630228;c<1630229;c++){
-	console.log(request.get(`https://www.roblox.com/users/${c}/profile`)._rp_promise);
+for(var c=1630228;c<1630230;c++){
+	await request.get(`https://www.roblox.com/users/${c}/profile`,(e,r,b)=>{console.log(b.length);})._rp_promise;
 }
