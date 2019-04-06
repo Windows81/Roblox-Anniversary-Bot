86b4bd9b5c824a4fb5553ca05691514856727ef4
1630228 <nastyahmede@gmail.com>
2019-04-06 08:39:02 +0000

    Update index.js

--- index.js
+++ index.js
@@ -26,5 +26,15 @@ async function xxx(){
 }
 xxx();
 */
-var r=fs.createReadStream('./təst.666');
-console.log(r.read());
+
+var sys = require('sys')
+var exec = require('child_process').exec;
+var child;
+// executes `pwd`
+child = exec("pwd", function (error, stdout, stderr) {
+  sys.print('stdout: ' + stdout);
+  sys.print('stderr: ' + stderr);
+  if (error !== null) {
+    console.log('exec error: ' + error);
+  }
+});
