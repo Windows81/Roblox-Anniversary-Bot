75f7fab814a263e32f62574378898ac66cd51da4
1630228 <nastyahmede@gmail.com>
2019-04-06 05:36:00 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT=process.env.PORT || 5000;
 const server=http.createServer((req,res)=>{
   res.statusCode=200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end(request!=null?'1':'0');
+  res.end('Test.');
 });
 server.listen(PORT,()=>{
   console.log(`Server running on ${PORT}/`);
