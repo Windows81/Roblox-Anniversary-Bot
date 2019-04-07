96bc5f063c334db02beabb5eb89f8cff1851a561
1630228 <nastyahmede@gmail.com>
2019-04-07 08:29:44 +0000

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ async function getFirstPlayerFromDate(base,dateInt){
 	var delta=0x1000,switched=false;
 	for(var c=base;true;c+=delta){
 		var jd=await joinD8(c);
-		console.log(c,jd);
+		console.log(c,jd,delta<0^jd>=dateInt,delta);
 		if(delta<0^jd>=dateInt)
 			if(c==base)delta*=-1;
 			else if(delta==1)return c;
