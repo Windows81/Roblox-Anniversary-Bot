6bc2e5de8a36e749caf690a0581734e62e2c38e4
1630228 <nastyahmede@gmail.com>
2019-04-07 08:39:16 +0000

    Update index.js

--- index.js
+++ index.js
@@ -34,14 +34,16 @@ function joinD8(id){
 async function getFirstPlayerFromDate(base,dateInt){
 	var delta=0x1000,switched=false;
 	for(var c=base;true;c+=delta){
-		var jd=await joinD8(c);
+		var jd;
+		do jd=await joinD8(c);
+		while(!jd&&c--);
 		if(delta<0^jd>=dateInt)
 			if(c==base)delta*=-1;
 			else if(delta==1)return c;
 			else if(delta==-1)return c+1;
 			else{switched=true;delta/=-2;}
 		else if(!switched)delta*=2;
-		console.log(c,jd,delta<0^jd>=dateInt,delta);
+		console.log(c,jd);
 	}
 }
 
